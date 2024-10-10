d2819e7a13ebc3c292ae40312e33ec53533b4a64
shu <chukichi915@gmail.com>
2024-10-10 14:31:24 +0900

    登録ができるようになった

---
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Users
+      class SessionsController < ApplicationController
+      end
+    end
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,11 @@ module Myapp
                        routing_specs: false
       g.factory_bot false
     end
+
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
+
+    config.i18n.default_locale = :ja
   end
 end
