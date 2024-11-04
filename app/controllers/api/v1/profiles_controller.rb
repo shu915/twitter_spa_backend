79295e720f4a171dee1b8780b2f13171930091b5
shu915 <chukichi915@gmail.com>
2024-11-04 16:01:58 +0900

    プロフィール画面と編集画面を作った

---
+++ app/controllers/api/v1/profiles_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ProfilesController < ApplicationController
+      before_action :authenticate_api_v1_user!
+
+      def show
+        user = User.find(params[:id])
+        render json: user.as_json.merge(
+          header_image_url: user.header_image.attached? ? url_for(user.header_image) : nil,
+          profile_image_url: user.profile_image.attached? ? url_for(user.profile_image) : nil
+        ), status: :ok
+      end
+
+      def update
+        user = User.find(params[:id])
+        if current_api_v1_user.id == user.id
+          user.profile_image.attach(params[:user][:profile_image]) if params[:user][:profile_image].present?
+          user.header_image.attach(params[:user][:header_image]) if params[:user][:header_image].present?
+          user.update(profile_params)
+          render json: user, status: :ok
+        else
+          render json: { error: 'Unauthorized' }, status: :unauthorized
+        end
+      end
+
+      private
+
+      def profile_params
+        params.require(:user).permit(:display_name, :bio, :location, :website, :profile_image, :header_image)
+      end
+    end
+  end
+end
