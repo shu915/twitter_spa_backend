6aaed65526174517249a8dd90282dcb2f189dfc5
shu915 <chukichi915@gmail.com>
2024-11-05 11:12:22 +0900

    rubocopをまた通した

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -32,15 +32,15 @@ module Api
       end
 
       def update_profile(user)
-          user.profile_image.attach(profile_params[:profile_image]) if profile_params[:profile_image].present?
-          user.header_image.attach(profile_params[:header_image]) if profile_params[:header_image].present?
+        user.profile_image.attach(profile_params[:profile_image]) if profile_params[:profile_image].present?
+        user.header_image.attach(profile_params[:header_image]) if profile_params[:header_image].present?
 
-          if user.update(profile_params)
-            render json: user.as_json.merge(
-              header_image_url: user.header_image.attached? ? url_for(user.header_image) : nil,
-              profile_image_url: user.profile_image.attached? ? url_for(user.profile_image) : nil
-            ), status: :ok
-          else
+        if user.update(profile_params)
+          render json: user.as_json.merge(
+            header_image_url: user.header_image.attached? ? url_for(user.header_image) : nil,
+            profile_image_url: user.profile_image.attached? ? url_for(user.profile_image) : nil
+          ), status: :ok
+        else
           render json: { error: user.errors.messages }, status: :unprocessable_entity
         end
       end
