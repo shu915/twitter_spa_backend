790d0fc8843d00cdf842b140dd6b1b193d02f04d
shu915 <chukichi915@gmail.com>
2024-11-08 10:00:33 +0900

    ツイートと返信のどちらかが削除されたら、関係が切れるということで落ち着いた

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,13 +4,11 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_one :image, dependent: :destroy
 
-  # rubocop:disable Rails/HasManyOrHasOneDependent
-  has_many :replies_as_parent, class_name: 'Reply', foreign_key: 'parent_tweet_id', inverse_of: :parent_tweet
+  has_many :replies_as_parent, class_name: 'Reply', foreign_key: 'parent_tweet_id', inverse_of: :parent_tweet, dependent: :destroy
   has_many :children, through: :replies_as_parent, source: :child_tweet
 
-  has_many :replies_as_child, class_name: 'Reply', foreign_key: 'child_tweet_id', inverse_of: :child_tweet
+  has_many :replies_as_child, class_name: 'Reply', foreign_key: 'child_tweet_id', inverse_of: :child_tweet, dependent: :destroy
   has_one :parent, through: :replies_as_child, source: :parent_tweet
-  # rubocop:enable Rails/HasManyOrHasOneDependent
 
   validates :content, presence: true, length: { maximum: 140 }
 end
