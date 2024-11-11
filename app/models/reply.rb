2af6acddcb517da4473900890e1e54cc72d9a011
shu915 <chukichi915@gmail.com>
2024-11-11 15:25:55 +0900

    ツイート詳細画面で、関連するツイートや返信をみれるようになった

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Reply < ApplicationRecord
-  belongs_to :parent_tweet, class_name: 'Tweet', inverse_of: :replies_as_parent
+  belongs_to :parent_tweet, class_name: 'Tweet', inverse_of: :replies_as_parent, counter_cache: true
   belongs_to :child_tweet, class_name: 'Tweet', inverse_of: :replies_as_child
 end
