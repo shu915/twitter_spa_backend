07b49fc93ec8ecc6b7aaec886ce76f4f1cc7f4b8
shu915 <chukichi915@gmail.com>
2024-12-06 12:37:43 +0900

    退会できるようになった

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,14 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[show] do
-        resources :tweets, only: %i[index]
-        resources :follows, only: %i[index create destroy]
-      end
-
       mount_devise_token_auth_for 'User', at: 'users', controllers: {
         registrations: 'api/v1/users/registrations',
         sessions: 'api/v1/users/sessions'
       }
+      resources :users, only: %i[show] do
+        resources :tweets, only: %i[index]
+        resources :follows, only: %i[index create destroy]
+      end
 
       resources :tweets, only: %i[index create show destroy] do
         resources :replies, only: %i[create]
